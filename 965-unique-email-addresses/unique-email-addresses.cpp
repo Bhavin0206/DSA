2086860f648c56a1f34b36f65c8b069a97173555
Bhavin Padhariya <100347903+Bhavin0206@users.noreply.github.com>
2026-06-27 18:06:38 +0530

    Time: 5 ms (68.02%) | Memory: 17.5 MB (82.34%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string> receiveMails;
+
+        for (int i = 0; i < emails.size(); i++) {
+            string addWords = "";
+            int at = emails[i].find('@');
+
+            for (int j = 0; j < emails[i].size(); j++) {
+
+                if (emails[i][j] == '.' && j < at) {
+                    continue;
+                }
+
+                if (emails[i][j] == '+' && j < at) {
+                    j = at;
+                }
+
+                addWords += emails[i][j];
+            }
+
+            receiveMails.insert(addWords);
+        }
+
+        return receiveMails.size();
+    }
+};
