cb1be60ca1049b323b7370926a42d4a3e3c04ec3
Bhavin Padhariya <100347903+Bhavin0206@users.noreply.github.com>
2026-07-02 23:03:17 +0530

    Time: 28 ms (58.55%) | Memory: 52 MB (22.64%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.cpp
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -1,25 +1,35 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+    vector<int> fairCandySwap(vector<int>& aliceSizes,
+                              vector<int>& bobSizes) {
         int aliceTotal = 0;
         int bobTotal = 0;
 
-        for (int x : aliceSizes) {
-            aliceTotal += x;
+        for (int box : aliceSizes) {
+            aliceTotal += box;
         }
 
-        for (int x : bobSizes) {
-            bobTotal += x;
+        for (int box : bobSizes) {
+            bobTotal += box;
+        }
+
+        int difference = (bobTotal - aliceTotal) / 2;
+
+        unordered_set<int> bobSet;
+
+        for (int box : bobSizes) {
+            bobSet.insert(box);
         }
 
         for (int aliceBox : aliceSizes) {
-            for (int bobBox : bobSizes) {
-                int newAliceTotal = aliceTotal - aliceBox + bobBox;
-                int newBobTotal = bobTotal - bobBox + aliceBox;
+            int bobBoxNeeded = aliceBox + difference;
 
-                if (newAliceTotal == newBobTotal) {
-                    return {aliceBox, bobBox};
-                }
+            if (bobSet.find(bobBoxNeeded) != bobSet.end()) {
+                return {aliceBox, bobBoxNeeded};
             }
         }
 
