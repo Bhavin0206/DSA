87a3178213f0f843d0b10234f226cb73b8704ca9
Bhavin Padhariya <100347903+Bhavin0206@users.noreply.github.com>
2026-07-05 16:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (34.78%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        int count=0;
+        unordered_set<int>store;
+
         for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                for(int k=j+1;k<nums.size();k++){
-                    if(i<j && j<k){
-                        if(nums[j]-nums[i]==diff && nums[k]-nums[j]==diff){
-                            count++;
-                        }
-                    }
-                }
+            store.insert(nums[i]);
+        }
+        int count=0;
+
+        for(int j=0;j<nums.size();j++){
+            int x=nums[j];
+            int numsJ=x+diff;
+            int numsK=x+2*diff;
+            if(store.find(numsJ)!=store.end() && store.find(numsK)!=store.end()){
+                count++;
             }
         }
         return count;
+
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         for(int k=j+1;k<nums.size();k++){
+        //             if(i<j && j<k){
+        //                 if(nums[j]-nums[i]==diff && nums[k]-nums[j]==diff){
+        //                     count++;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
         
     }
 };
