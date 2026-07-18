f7177dd21e87b494edb50ca7024b928721fc05a8
Bhavin Padhariya <100347903+Bhavin0206@users.noreply.github.com>
2026-07-18 23:44:43 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (80.93%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -12,35 +12,4 @@ public:
         }
         return slow;
     }
-
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //       int slow = 0;
-
-    // for (int fast = 1; fast < nums.size(); fast++) {
-    //     if (nums[fast] != nums[slow]) {
-    //         swap(nums[slow], nums[fast]);
-    //         slow++;
-    //         nums[slow]=nums[fast];
-    //     }
-       
-    // }
-    //    return slow+1;  
-    // }
 };
