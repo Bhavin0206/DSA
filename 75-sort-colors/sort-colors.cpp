39d7828622295438613fda0cbeb3dd17f4282d5c
Bhavin Padhariya <100347903+Bhavin0206@users.noreply.github.com>
2026-07-24 22:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.39%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            int min_ind=i;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[j]<nums[min_ind]){
+                    min_ind=j;
+                }   
+            }
+            swap(nums[min_ind],nums[i]);
+        }
+        
+    }
+};
